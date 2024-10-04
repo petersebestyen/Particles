628b26a6f05e9749a13c4e7814b8ebd882a932d6
Peter Sebestyen <peter.sebestyen@mac.com>
2024-10-04 21:11:28 +0200

    Initial Commit

---
+++ Particles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Particles
+//
+//  Created by Péter Sebestyén on 2024.10.04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Particles/ParticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParticlesApp.swift
+//  Particles
+//
+//  Created by Péter Sebestyén on 2024.10.04.
+//
+
+import SwiftUI
+
+@main
+struct ParticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
